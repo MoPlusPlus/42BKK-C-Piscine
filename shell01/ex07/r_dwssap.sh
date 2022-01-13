bfa51a2ace86502d35f00a3a52e64a716bbf392b
Chanon Upatham <cupatham@z1t11c4.42bangkok.com>
2022-01-13 17:31:58 +0700

    100 shell00 shell01

--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,12 +1,23 @@
-#cat /etc/passwd
-#remove comment
-#every other line starting from the second line
-#reversing each login
-#sorted in reverse alphabetical order
-#and keeping only logins between FT_LINE1 and FT_LINE2 included
-#and they must separated by ", "(without quotationmarks)
-#and the output must end with a "."
+cat /etc/passwd | \
 
- cat /etc/passwd | sed -e 's/#.*//' -e 's/[ ^I]*$//' -e '/^$/ d' -e 's/:.*//g' \
- | tr '\n' ',' | rev | tr "," "\n" | sort -r | tr "\n" ","  \
- | sed -e 's/, */, /g' -e 's/.$//' -e 's/.\{3\}$//'
+ grep -e '^#' -v | \
+
+ awk 'NR%2==0'| \
+
+ sed -e "s/[:*].*$/ /" | \
+
+ rev | \
+
+ sort -r | \
+
+ sed -n "$FT_LINE1,$FT_LINE2 p" | \
+
+ tr '\n' ', ' | \
+
+ sed -e "s/^, //" | \
+
+ sed -e "s/^ //" | \
+
+ sed -e "s/,$/./" | 
+
+ tr -d '\n'
--- shell01/ex08/add_chelou.sh
+++ shell01/ex08/add_chelou.sh
@@ -1,5 +1,19 @@
-export FT_NBR1=\"\"!\"\"!\"\"!\"\"!\"\"!\"\"
+echo $FT_NBR1 + $FT_NBR2 | \
 
-export FT_NBR2=rcrdmddd
+  sed 's/\\/1/g' | \
 
-echo "ibase=5; obase=D; $(echo "$FT_NBR1 + $FT_NBR2" | tr \'\\ 01 | tr \"\?\! 234 | tr mrdoc 01234)" | bc | tr 0123456789ABC 'gtaio luSnemf'
+  sed 's/?/3/g' | \
+
+  sed 's/!/4/g' | \
+
+  sed "s/\'/0/g" | \
+
+  sed "s/\"/2/g" | \
+
+  tr "mrdoc" "01234" | \
+
+  xargs echo "ibase=5; obase=23;" | \
+
+  bc | \
+
+  tr "0123456789ABC" "gtaio luSnemf"
